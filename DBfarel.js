05ac83b159b96acc24149e8a6c0cede097693a24
Yermieee-Wrld <ambonpicapica555@gmail.com>
2025-06-23 10:02:04 +0800

    DBfarel.js

--- DBfarel.js
+++ DBfarel.js
@@ -1,27 +1,83 @@
-{
-  "data": [
-    "6288987555100",
-    "6287890234392",
-    "6281376462935",
-    "6285810091758",
-    "6287873540959",
-    "6283122012870",
-    "6287762745232",
-    "6289675799409",
-    "6287873540959",
-    "6285606754374",
-    "6281371531105",
-    "6287762745232",
-    "62895415235000",
-    "6283801933888",
-    "6289635801056",
-    "628980639595",
-    "6282181804515",
-    "6282160476046",
-    "6281228524770",
-    "6287873540959",
-    "6283133898447",
-    "6283898813980"
-  ]
- }
 
+
+
+
+
+
+
+
+
+{
+  "users": {
+    "6287890234392@s.whatsapp.net": { 
+    "registered": true
+    },
+    "6281376462935@s.whatsapp.net": { 
+    "registered": true
+    },
+    "6285810091758@s.whatsapp.net": { 
+    "registered": true
+    },
+    "6287873540959@s.whatsapp.net": { 
+    "registered": true
+    },
+    "6283122012870@s.whatsapp.net": { 
+    "registered": true
+    },
+    "6287762745232@s.whatsapp.net": { 
+    "registered": true
+    },
+    "6289675799409@s.whatsapp.net": { 
+    "registered": true
+    },
+    "6287873540959@s.whatsapp.net": { 
+    "registered": true
+    },
+    "6285606754374@s.whatsapp.net": { 
+    "registered": true
+    },
+    "6281371531105@s.whatsapp.net": { 
+    "registered": true 
+    }, 
+    "6287762745232@s.whatsapp.net": { 
+    "registered": true 
+    }, 
+    "6289699890494@s.whatsapp.net": {
+    "registered": true
+    },
+    "62895415235000@s.whatsapp.net": { 
+    "registered": true 
+    },
+    "6283801933888@s.whatsapp.net": { 
+    "registered": true 
+    },
+    "6289635801056@s.whatsapp.net": {
+    "registered": true 
+    },
+    "628980639595@s.whatsapp.net": { 
+    "registered": true 
+    },
+    "6282181804515@s.whatsapp.net": { 
+    "registered": true 
+    }, 
+    "6282160476046@s.whatsapp.net": { 
+    "registered": true 
+    },
+    "6281228524770@s.whatsapp.net": { 
+    "registered": true 
+    }, 
+    "6287873540959@s.whatsapp.net": { 
+    "registered": true 
+    },
+    "6283133898447@s.whatsapp.net": { 
+    "registered": true 
+    },
+    "6283898813980@s.whatsapp.net": { 
+    "registered": true 
+    },
+    "6288987555100@s.whatsapp.net": { 
+    "registered": true 
+    }
+    
+  }
+}
